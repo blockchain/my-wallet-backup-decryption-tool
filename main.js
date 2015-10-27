9a2bda96c2491c786590bf15f76eb4d4983dfea7
Justin Tormey <jrtormey@gmail.com>
2015-10-27 00:26:11 -0400

    add custom menu

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var app           = require('app')
   , BrowserWindow = require('browser-window')
+  , Menu          = require('menu')
   , env           = require('node-env-file');
 
 var mainWindow = null;
@@ -19,6 +20,26 @@ app.on('ready', function() {
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+
+  var template = [{
+    label: "Application",
+    submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "CmdOrCtrl+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" }
+    ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
 
 function loadEnv(file) {
